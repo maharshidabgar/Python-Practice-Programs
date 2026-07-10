b37eb8efa6dcdb19d2540b72671765e9546d4edb
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-10 11:05:26 +0530

    Update first.py

--- BasicsOfPython/first.py
+++ BasicsOfPython/first.py
@@ -3,3 +3,7 @@ print("Jay SwamiNarayan...")
 print("Maharshi is my Name.")
 
 print("My age is 21.")
+
+print(245)
+
+print(245 + 4)
---
+++ BasicsOfPython/var.py
@@ -0,0 +1,11 @@
+name = "Maharshi" # String
+
+age = -21 # Integer
+
+price = 55.45 # Float
+
+age2 = age
+
+print(age2)
+
+print(type(age))  
