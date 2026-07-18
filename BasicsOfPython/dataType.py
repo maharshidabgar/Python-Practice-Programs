b556f47d11ad2a48605b33d0023e280f215d7edc
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-18 11:26:20 +0530

    Add str.py dataType.py

---
+++ BasicsOfPython/dataType.py
@@ -0,0 +1,8 @@
+age = 21
+
+old = False
+
+a = None
+
+print(type(old))
+print(type(a))
---
+++ BasicsOfPython/str.py
@@ -0,0 +1,7 @@
+name1 = "Chirag"
+name2 = 'Maharshi'  # Also same " "=''=''' in String Declaration
+name3 = '''Shivam'''
+
+print(name1)
+print(name2)
+print(name3)
