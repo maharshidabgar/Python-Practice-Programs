6d6af6937ed5e1be93319dca2fdf4045c4cc1b06
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-22 11:06:17 +0530

    Add if_elif.py

---
+++ BasicsOfPython/if_eleif.py
@@ -0,0 +1,10 @@
+light = input("Light : ")
+
+if (light == "red"):
+    print("Stop !")
+elif (light == "yellow"):
+    print("Look !")
+elif(light == "green"):
+    print("Go boss !")
+else:
+    print("Light is Broken...")
