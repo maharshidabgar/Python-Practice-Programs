6effcc5fb24c2d6ca70873640292ea05a46c6ddf
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-23 19:02:28 +0530

    M slicing.py

--- BasicsOfPython/slicing.py
+++ BasicsOfPython/slicing.py
@@ -1,7 +1,9 @@
 str = "OmNamahShivay"
 
-print(str[1:])
+print(str[1:]) # Start se suru karke Last tak puri String
 
-print(str[0:5])
+print(str[0:5]) 
 
 print(str[6:11])
+
+print(str[:]) # Full String Print
