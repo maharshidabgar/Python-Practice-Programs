0e6218b171eca8f1606508709e60d234e41dac7c
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-23 12:48:41 +0530

    Modified ip.py

--- BasicsOfPython/ip.py
+++ BasicsOfPython/ip.py
@@ -6,3 +6,10 @@ print("Welcome",name)
 
 # INT IP
 
+age = int(input("Enter Your Age : "))
+
+print("Your Age is ",age)
+
+marks = float(input("Enter Your Marks beta : "))
+
+print("Your Marks is ",marks)
