83ff7dd019e13284655094267be17d10a848a0e3
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-23 16:43:48 +0530

    Modified pq3.py

--- BasicsOfPython/pq3.py
+++ BasicsOfPython/pq3.py
@@ -2,4 +2,4 @@ fnum = float(input("Enter First Number : "))
 
 snum = float(input("Enter Second Number : "))
 
-print("Aveage of Two Floating Point number is ",fnum+snum/2)
+print("Aveage of Two Floating Point number is ",(fnum+snum)/2)
