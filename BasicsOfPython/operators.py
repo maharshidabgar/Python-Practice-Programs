9e61697707cc36f2cf9f78310f6ef383589066bb
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-23 11:09:53 +0530

    Add operators.py

---
+++ BasicsOfPython/operators.py
@@ -0,0 +1,9 @@
+a = 5 
+b = 2
+
+print(a + b) # Summation
+print(a - b) # Subtraction 
+print(a * b) # Multiplication
+print(a / b) # Division
+print(a % b) # Modulo = Remainder = Shesh
+print(a ** b) # Power of B = 2
