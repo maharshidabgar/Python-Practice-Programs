f516a82d5cdb96908d96b473dba027256761fcde
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-23 11:21:10 +0530

    Modifiedd operators.py

--- BasicsOfPython/operators.py
+++ BasicsOfPython/operators.py
@@ -1,3 +1,5 @@
+# Arithmetic Operators
+
 a = 5 
 b = 2
 
@@ -7,3 +9,37 @@ print(a * b) # Multiplication
 print(a / b) # Division
 print(a % b) # Modulo = Remainder = Shesh
 print(a ** b) # Power of B = 2
+
+# Comparison / Relational Operators
+
+c = 50
+d = 44
+
+print(c == d) # False
+print(c != d) # True
+print(c > d) # True
+print(c >= d) # True
+print(c < d) # False
+print(c <= d) # False
+
+# Assignment Operators
+
+num = 10 
+
+num = num + 10 # 10 + 10 => 20
+print("Num : ",num)
+
+num += 5 # Sum Equal To
+print("Num is now : ",num)
+
+num -= 10 # Sub Equal to
+print("Num Sub 10 : ",num)
+
+num *= 2 # Multipky Equal to
+print("Num Mul : ",num)
+
+num /= 4 # Division Equal to
+print("Division = : ",num)
+
+num **= 2 # Power Off Equal to
+print("Power of = : ",num)
