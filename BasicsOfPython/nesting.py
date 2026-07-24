5865a2601a7215f41f47c41c82370b9bfdbddbf0
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-24 11:23:42 +0530

    Add nesting.py

---
+++ BasicsOfPython/nesting.py
@@ -0,0 +1,17 @@
+age = 65
+
+# Nesting = if inside if okk
+
+if (age >= 18):
+
+    if(age >= 80):
+
+        print("Cannot Drive")
+
+    else:
+
+        print("Can Drive")
+
+else:
+
+    print("Cannot Drive")
