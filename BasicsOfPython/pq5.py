8e19483f3d6ff4ae602f474d7ab418e118b421a1
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-24 10:38:53 +0530

    M pq5.py

--- BasicsOfPython/pq5.py
+++ BasicsOfPython/pq5.py
@@ -1,3 +1,7 @@
 fname = (input("Enter Your First Name : "))
 
-print(len(fname))
+print("Lenght of your name is", len(fname))
+
+str = "Avo avo MAma $500 lavela and me $600 pacha apya $100 dollar vadharya..."
+
+print("In this String $ sign occur", str.count("$"), "Times...")
