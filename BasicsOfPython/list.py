125e4ead8bcfd5440c631ab7d73e50a3378cde5d
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-24 12:12:42 +0530

    Add list.py

---
+++ BasicsOfPython/list.py
@@ -0,0 +1,17 @@
+# List Declaration
+
+marks = [12.5, 98.9, 87.5, 95.2, 66.4, 45.3]
+
+print(marks)
+
+print(type(marks))
+
+print(marks[0]) # printing the 0 index marks in List
+
+print(marks[2])
+
+print(len(marks)) # Length of List
+
+marks[1] = 96.5 # Changes 1 indexed 98.9 to 96.5 permenantly in List
+
+print(marks)
