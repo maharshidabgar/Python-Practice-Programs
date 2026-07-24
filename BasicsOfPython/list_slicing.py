b7bc9dbf54567cccf703d66775576810d6db3735
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-24 16:59:13 +0530

    Add list_slicing.py

---
+++ BasicsOfPython/list_slicing.py
@@ -0,0 +1,11 @@
+marks = [33, 95, 67, 74, 82]
+
+# List - Slicing
+
+print(marks[1:4])
+
+print(marks[:4]) # 0 to 4 and 4rth index is not print
+
+print(marks[1:]) # 1st start and full len(marks)
+
+print(marks[-4:-2])
