0a950d2e1a4b298a09f1e0e984fe65c2081d9797
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-25 11:18:27 +0530

    Add list_methods.py

---
+++ BasicsOfPython/list_methods.py
@@ -0,0 +1,33 @@
+list = [2,1,3,4,6]
+
+# List - Append
+
+list.append(5)
+
+print(list)
+
+# List - Sort
+
+list.sort()
+
+print(list)
+
+# List - Reverse - Sort
+
+list.sort(reverse = True)
+
+print(list)
+
+# List - Reverse
+
+list2 = [4,5,6] # [6,5,4]
+
+list2.reverse()
+
+print(list2)
+
+# List - Inserting
+
+list2.insert(0,9)
+
+print(list2) 
