efe82492e1a34f6b9ffd6b3f8f9d54d39137df40
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-26 11:57:00 +0530

    Add pq9.py

---
+++ BasicsOfPython/pq9.py
@@ -0,0 +1,7 @@
+movi1 = input("Enter your 1st Fav Movie : ")
+movi2 = input("Enter your 2nd Fav Movie : ")
+movi3 = input("Enter your 3rd Fav Movie : ")
+
+listofMovie = [movi1, movi2, movi3]
+
+print("Your Favourite 3 Movies List \n : ", listofMovie)
