66546e4489385bb3db84cbe24710517ef89adfde
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-26 10:51:21 +0530

    tuple.py

--- BasicsOfPython/tuple.py
+++ BasicsOfPython/tuple.py
@@ -1,3 +1,15 @@
 tup = (87, 45, 33, 26, 76)
 
-print(tup)  
+print(type(tup)) 
+
+print(tup[0])
+print(tup[1]) # Access of Tuple is Allowed
+
+# but
+
+# tup[0] = 92 # This not Allowed bcz Tuple Operator 
+
+# is not Allowed Item Assignmet
+
+
+
