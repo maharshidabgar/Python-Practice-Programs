120eb6468f12841f00345bb06016254a9c557363
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-26 15:48:01 +0530

    M pq11.py

--- BasicsOfPython/pq11.py
+++ BasicsOfPython/pq11.py
@@ -1,3 +1,13 @@
-grades = ("c","D","A","A","B","B","A")
+grades = ("C","D","A","A","B","B","A")
+
+# Count Method
 
 print(grades.count("A"))
+
+# Sorting Method for List
+
+grade = ["C","D","A","A","B","B","A"]
+
+grade.sort()
+
+print(grade)
