7b2fa0a9d13a1ef4807f9461c259851225411ab5
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-26 17:08:02 +0530

    M nested_dict.py

---
+++ BasicsOfPython/nested_dict.py
@@ -0,0 +1,20 @@
+student = {
+    "name": "Rahul Shah",
+
+    # Nested Dictionary
+
+    "Subjects": {
+        "Physics": 79,
+        "Biology": 58,
+        "Chemistry": 76,
+        "English": 82,
+        "Sanskrit": 56,
+        "Computer": 92,
+    }
+}
+
+print(student) # Printing Full Dict - Student
+
+print(student["Subjects"]) # Print Nested Dict Subjects
+
+print(student["Subjects"]["Chemistry"]) # Print Subjects'S Chemistry Marks
