854fff68f1374476969b374bfea37487e045c2f1
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-27 11:48:39 +0530

    Add set.py

---
+++ BasicsOfPython/set.py
@@ -0,0 +1,22 @@
+set = {1, 2, 2, 3, 4}
+
+# 2 is two times but print only 1 time
+
+print(set) # {1,2,3,4}
+
+print(type(set))
+
+# Stores Immutable things like - int, float, str, bool
+
+newSet = {"Manav", True, False, 78.54, 45}
+
+print(newSet)
+
+print(len(newSet)) # Total Number of Items
+
+# do not store List & Dictionary in set bcz It Mutable
+
+# Set is Immutable 
+
+# Duplicate Value Ignored
+# Unordered
