75f6339fed028e9931877592adf60869e97dbfe1
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-28 12:12:25 +0530

    M pq2.py

--- BasicsOfPython/pq2.py
+++ BasicsOfPython/pq2.py
@@ -1,3 +1,33 @@
+"""
+===========================================================
+🐍 Python Practice Question #02
+
+Question:
+Write a Python program to input the side of a square from
+the user and calculate its area.
+
+Objective:
+- Learn how to take user input.
+- Perform arithmetic calculations.
+- Calculate the area of a square using the formula.
+- Display the result using the print() function.
+
+Formula:
+Area of Square = side × side (side²)
+
+Concepts Used:
+- input()
+- int()
+- Arithmetic Operators (*, **)
+- print()
+
+Author : Maharshi Dabgar
+Language : Python 3
+Repository : Python-Practice-Programs
+
+===========================================================
+"""
+
 side = int(input("Enter side of a Square : "))
 
-print("Area of Square is ",side*side) # side ** 2
+print("Area of Square is", side * side)  # side ** 2
