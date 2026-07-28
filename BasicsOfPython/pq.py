0103573e59670f5f33bb235226f8a4efe3655e73
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-28 12:09:28 +0530

    M pq1.py

--- BasicsOfPython/pq.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input("Enter Num1 : "))
-
-num2 = int(input("Enter Num2 : "))
-
-print("The Sum of 2 Numbers is : ", num1 + num2)
---
+++ BasicsOfPython/pq1.py
@@ -0,0 +1,31 @@
+"""
+===========================================================
+🐍 Python Practice Question #01
+
+Question:
+Write a Python program to input two numbers from the user
+and print their sum.
+
+Objective:
+- Learn how to take user input.
+- Perform basic arithmetic operations.
+- Display the result using the print() function.
+
+Concepts Used:
+- input()
+- int()
+- Arithmetic Operator (+)
+- print()
+
+Author : Maharshi Dabgar
+Language : Python 3
+Repository : Python-Practice-Programs
+
+===========================================================
+"""
+
+num1 = int(input("Enter Num1 : "))
+
+num2 = int(input("Enter Num2 : "))
+
+print("The Sum of 2 Numbers is : ", num1 + num2)
