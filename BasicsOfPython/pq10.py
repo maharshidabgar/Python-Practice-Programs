0ac7e3cd0be8b399303526efb861836ccfde7ff1
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-28 12:29:49 +0530

    M pq10.py

--- BasicsOfPython/pq10.py
+++ BasicsOfPython/pq10.py
@@ -1,9 +1,38 @@
-list1 = ["m","a","o","m"]
+"""
+===========================================================
+🐍 Python Practice Question #10
+
+Question:
+Write a Python program to check whether a list is a
+palindrome or not.
+
+Objective:
+- Learn how to copy a list.
+- Reverse a list using the reverse() method.
+- Compare two lists to determine whether they are
+  palindromes.
+
+Concepts Used:
+- List
+- copy()
+- reverse()
+- if-else
+- Comparison Operator (==)
+- print()
+
+Author : Maharshi Dabgar
+Language : Python 3
+Repository : Python-Practice-Programs
+
+===========================================================
+"""
+
+list1 = ["m", "a", "o", "m"]
 
 copy_list1 = list1.copy()
 copy_list1.reverse()
 
-if(copy_list1 == list1):
+if copy_list1 == list1:
     print("Palindrome...")
 else:
     print("Not Palindrome...")
