595e5ef76dbac0d364ab8fb688850e0bc77e1ff5
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-28 11:37:57 +0530

    M mathsSet.py

--- BasicsOfPython/mathsSet.py
+++ BasicsOfPython/mathsSet.py
@@ -1,2 +1,15 @@
 # Union Method
 
+set1 = {1, 2, 3}
+
+set2 = {2, 3, 4}
+
+print(set1.union(set2)) # {1, 2, 3, 4}
+
+# union set is New Value
+
+# set1 & set2 is same as Assigning time
+
+# Intersection - Method
+
+print(set1.intersection(set2)) # {2, 3}
