cd0dff24395812942dd18e943fb8bd6c0a8c484e
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-28 10:36:35 +0530

    M set_methods.py

---
+++ BasicsOfPython/set_methods.py
@@ -0,0 +1,20 @@
+
+set = {11, "Hellow", 45, 78, 72.99}
+
+# Add - Method for Element
+
+set.add(74)
+
+# Remove - Method for Element
+
+set.remove(11)
+
+# Empties the Set
+
+set.clear()
+
+# Removes a Random Value
+
+set.pop()
+
+print(set)
