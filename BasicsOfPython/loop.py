5276b30c549eae9a661cac16f6279a1156d82d49
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-28 16:30:25 +0530

    Add loop.py

--- BasicsOfPython/loop.py
+++ BasicsOfPython/loop.py
@@ -1,7 +1,52 @@
-count = 1
+count = 1 # Variable Assignment = Iterators
 
-while count <= 5:
-    print("Hello")
+while count <= 9: # Loop Declaration with Condition = Iteration
+
+    print("Hello") # Some Work
+
+    # Incr / Decr Work
+
     count += 1 # 5 tak Print hoga baad me 6 jese aayega Loop Khatam 
 
 print(count) # 6
+
+m = 1 # Initialization
+
+while m <= 10: # Termination
+
+    print("Jay SwamiNarayan",m)
+
+    m += 1 # Increment / Decrement Work
+
+# Print Numbers form 1 to 15 in Reverse
+
+i = 15
+ 
+while i >= 1:
+
+    print(i)
+
+    i -= 1
+
+print("Loop Ended")
+
+# Ascending order 1 to 45
+
+n = 1
+
+while n <= 45:
+
+    print(n)
+    n += 1
+
+print("Loop Khatam !")
+
+# Infinite Loop 
+
+# j = 4
+
+while j < 6:
+    print(j)
+    j -= 1
+
+print("Ye Kabhi Print hi Nahi hoga !!!")
