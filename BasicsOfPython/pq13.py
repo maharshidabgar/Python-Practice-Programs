80b30470de13160094a3f95edda2b20ba0ef25c7
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-28 11:48:32 +0530

    M pq13.py

---
+++ BasicsOfPython/pq13.py
@@ -0,0 +1,8 @@
+'''You are given a  list of Subjects for students. AAssume one 
+classroom is required for 1 subject. How many classrooms are needed
+by all students.?'''
+
+subjects = {"Python", "Java", "C++", "Python", "JavaScript",""
+            "Java", "Python", "C++", "C"}
+
+print(len(subjects))
