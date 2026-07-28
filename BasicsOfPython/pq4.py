510ed490b774124c4062f9d498d30ba9d8f34c53
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-28 12:15:09 +0530

    M pq4.py

--- BasicsOfPython/pq4.py
+++ BasicsOfPython/pq4.py
@@ -1,9 +1,35 @@
-a = int(input("Enter a Num : "))
+"""
+===========================================================
+🐍 Python Practice Question #04
 
+Question:
+Write a Python program to input two integers from the user
+and check whether the first number is greater than or equal
+to the second number.
+
+Objective:
+- Learn how to compare two numbers.
+- Understand relational operators.
+- Use conditional statements to display the result.
+
+Concepts Used:
+- input()
+- int()
+- Relational Operator (>=)
+- if-else
+- print()
+
+Author : Maharshi Dabgar
+Language : Python 3
+Repository : Python-Practice-Programs
+
+===========================================================
+"""
+
+a = int(input("Enter a Num : "))
 b = int(input("Enter b Num : "))
 
-if (a >= b):
+if a >= b:
     print("True")
 else:
     print("False")
-
