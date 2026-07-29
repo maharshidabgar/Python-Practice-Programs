d451ff328da4cef3a8b8e1e8b7fee12fb8a868d6
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-29 16:08:54 +0530

    Add range.py

--- BasicsOfPython/range.py
+++ BasicsOfPython/range.py
@@ -1,6 +1,23 @@
-seq = range(10)
-                 # Ending Number is Not Included
-for i in seq:
+
+for i in range(10): # 10 is only stoping condition user Give
 
     print(i)
 
+for i in range(2, 10): # range(start, stop) 
+
+    print(i)       # By Default range(start=0, stop=user, step=1)
+
+
+    # Starting Optional
+
+    # Stoping Given by User
+
+    # Step Optional no dyo toy Jate thaay jaay
+
+for i in range(4, 15, 2):
+
+    print(i)
+
+for n in range(3, 100, 4): # 3 se Start, 100 tak jao 99 print hoga, 4 Step se chalo
+
+    print(n)
