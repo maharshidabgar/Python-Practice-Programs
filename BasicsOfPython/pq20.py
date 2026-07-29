97151e70e967886bb774e143f8fa3a7355e86234
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-29 16:22:43 +0530

    Add pq20.py

---
+++ BasicsOfPython/pq20.py
@@ -0,0 +1,21 @@
+# using for & range
+
+# 1. Print numbers form 1 to 100.
+
+for i in range(1, 101, 1):
+
+    print(i)
+
+# 2. Print numbers from 100 to 1.
+
+for r in range(100, 0, -1):
+
+    print(r)
+
+# 3. Print the Multiplication table of a number n.
+
+n = int(input("Enter a Number : "))
+
+for m in range(1, 11):
+
+    print(n * m)
