4ef89f28152d343f0a94a0f9275b952a4aaa9d97
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-29 10:36:59 +0530

    Add continue_loop.py

--- BasicsOfPython/continue_loop.py
+++ BasicsOfPython/continue_loop.py
@@ -9,4 +9,18 @@ while i <= 5:
         continue # Skip for Printing 3
 
     print(i)
+    print(" ")
     i += 1
+
+e = 1
+
+while e <= 10:
+
+    if (e % 2 != 0): # Even Number Print
+
+        e += 1
+
+        continue
+
+    print(e)
+    e += 1
