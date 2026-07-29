b539031589b1075ec56ac912e44aab4194f1ff03
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-29 13:27:41 +0530

    Add for_loop.py

---
+++ BasicsOfPython/for_loop.py
@@ -0,0 +1,25 @@
+list = [1, 2, 3]
+
+for el in list: # el is List'S Element Name we are Give any name 
+
+    print(el)
+
+# With Else
+
+for el in list:
+
+    print(el)
+
+else:
+
+    print("End")
+
+vagies = ["Potato", "Brinjal", "Cucumber", "LadyFinger"]
+
+for val in vagies:
+
+    print(val)
+
+else:
+
+    print("End")
