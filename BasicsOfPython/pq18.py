f0fae1972c0b98b8440243138e0bf1bcd9561e94
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-29 15:21:03 +0530

    Add pq18.py

---
+++ BasicsOfPython/pq18.py
@@ -0,0 +1,11 @@
+# Using For loop Print the elements of the Following list using a Loop.
+
+num = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+for val in num:
+
+    print(val)
+
+else:
+
+    print("End")
