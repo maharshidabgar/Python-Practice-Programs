ff418272677123b0a8b9b21af58a2d9a747ade17
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-30 10:52:19 +0530

    Add functions.py

---
+++ BasicsOfPython/functions.py
@@ -0,0 +1,13 @@
+def calc_sum(a, b): # Parameter = IP Values / var_names
+
+    sum = a + b # Operation
+
+    print(sum) # only print inside
+
+    return sum # Outside print / OP value
+
+calc_sum(5, 6) # Function Calling => Work Assigning the var_values 
+
+# upper declare a, b
+
+
