8afe94683eba44b81a7cff4ba1ebc63823b60a4f
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-30 10:25:56 +0530

    M pq22.py

--- BasicsOfPython/pq22.py
+++ BasicsOfPython/pq22.py
@@ -1,5 +1,12 @@
 # WAP to find the Factorial of first n(Natural) numbers. (using for)
 
-n = 4
+n = 5
 
-fact 
+fact = 1
+
+for i in range(1, n+1):
+    fact *= i
+
+print("Factorial = ", fact)
+
+       # fact of 4 => 1*2*3*4 = 24
