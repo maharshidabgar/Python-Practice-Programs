80565c0aa0758469081c1c1d825ebd273a4121ec
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-30 12:42:21 +0530

    Add pq23.py

---
+++ BasicsOfPython/pq23.py
@@ -0,0 +1,15 @@
+# Write a Function print the length of a list. (list is the parameter)
+
+cities = ["Delhi", "Gurugram", "Noida", "Pune", "Amreli", "Mumbai"]
+
+heroes = ["Thor", "Ironman", "Captain America", "Shaktiman"]
+
+def print_len(cities):
+
+    print(len(cities))
+
+    return len(cities)
+
+print_len(cities)
+
+print_len(heroes)
