876b23dad7a1b41fa499bee834278e8becb75362
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-30 12:08:13 +0530

    M functions.py

--- BasicsOfPython/functions.py
+++ BasicsOfPython/functions.py
@@ -10,4 +10,28 @@ calc_sum(5, 6) # Function Calling => Arguments = var_values apply
 
 # upper declare a, b
 
+# Function's Work is Sum and we are AnyTime only Call and Work with
+# different Values 
 
+calc_sum(7, 7) # Print 14
+
+calc_sum(8, 5) # Print 13
+ 
+# Function - definition
+def mul_val(n, m): # Parameters
+
+    return n + m
+
+sum = mul_val(54588,2566)
+
+print(sum)
+
+# Printing Strinng
+
+def printing():
+
+    print("Jay SwamiNarayan")
+
+printing()
+printing()
+printing()
