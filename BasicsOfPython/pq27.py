5b16a8ebd76289465a7229a41881af74230eda25
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-30 16:31:58 +0530

    Add pq27.py

---
+++ BasicsOfPython/pq27.py
@@ -0,0 +1,15 @@
+# WAF to enter a number and Check number is odd or even...
+
+def check_evenOdd(num):
+
+    if num % 2 == 0:
+
+        print("Even-Num")
+
+    else:
+
+        print("Odd-Num")
+
+n = int(input("Enter a Number : "))
+
+check_evenOdd(n)
