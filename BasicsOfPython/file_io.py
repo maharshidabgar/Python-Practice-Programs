0893121519a3ebae5656daeaef12ea67f1807e9a
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-31 17:42:46 +0530

    M file_io.py

--- BasicsOfPython/file_io.py
+++ BasicsOfPython/file_io.py
@@ -5,9 +5,26 @@ data = f.read(4)      # Read the entire file
 
 print(data)
 
-print(type(data))
+print(type(data)) # Type of demo.txt data
 
-f.close()            # Close the file
+f.close()       # Close the file
 
 
+fo = open("demo.txt","r+") 
+
+line1 = fo.readline() # Read full Line
+
+print(line1)
+
+line2 = fo.readline() 
+
+print(line2)
+
+line3 = fo.readline()
+
+print(line3)
+
+f.close()
+
+# 
 
