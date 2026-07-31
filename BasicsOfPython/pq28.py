572b81545a53296a838a979861cc3e3501f38275
maharshidabgar <mahrshidabgar173@gmail.com>
2026-07-31 10:28:51 +0530

    Add pq28.py

--- BasicsOfPython/pq28.py
+++ BasicsOfPython/pq28.py
@@ -1,3 +1,13 @@
 # Write a Recursive function to calculate the sum of first n natural numbers.
 
-def 
+def cal_sum(n):
+
+    if (n == 1): # Base - Case
+
+        return 1
+
+    return n + cal_sum(n-1) # Recursive - Call
+
+n = int(input("Enter a Number : "))
+
+print("Sum =",cal_sum(n))
