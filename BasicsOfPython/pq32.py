13fce1205b38b7bf77e7e534b6a7c6047c886faa
maharshidabgar <mahrshidabgar173@gmail.com>
2026-08-06 11:15:01 +0530

    Add pq32.py

---
+++ BasicsOfPython/pq32.py
@@ -0,0 +1,16 @@
+# Search if the word "learning" exists in the file or not.
+
+word = "learning"
+
+with open("practice.txt", "r") as f:
+
+    data = f.read()
+
+    if(data.find(word) != -1):
+
+        print("Found...")
+
+    else:
+
+        print("Not found !")
+
