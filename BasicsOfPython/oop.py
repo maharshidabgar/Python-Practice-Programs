15429e87e1f5f079356305987b03b2a0a421b52f
maharshidabgar <mahrshidabgar173@gmail.com>
2026-08-07 11:35:57 +0530

    Add oop.py

---
+++ BasicsOfPython/oop.py
@@ -0,0 +1,40 @@
+# class = BluePrint of Objects
+
+# Object = Actual module Created from BluePrint
+
+# EX... of Declaration
+
+class Student:
+
+    pass
+
+s1 = Student()
+
+s2 = Student()
+
+# EX...of Showroom
+
+class Cars:
+
+    color = "Blue"
+    model = 2024
+
+c1 = Cars()
+c2 = Cars()
+
+print(c1.color)
+print(c2.model)
+
+# EX... of Mobile
+
+class Mobile:
+    pass
+
+brand = Mobile()
+price = Mobile()
+
+brand = "SamSang", "Nokia"
+price = 25000, 35000
+
+print(brand)
+print(price)
