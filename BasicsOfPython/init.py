2c4c57d49e0acf3a3ae852f1b8d1e058e347e3c0
maharshidabgar <mahrshidabgar173@gmail.com>
2026-08-08 12:07:09 +0530

    M init.py

--- BasicsOfPython/init.py
+++ BasicsOfPython/init.py
@@ -1,10 +1,13 @@
-class Stud:
+class student:
 
     def __init__(self, fullname):
 
         self.name = fullname
 
-s1 = Stud("Maharshi")
-print(s1.name)
+        print("Adding new Student in Database...")
 
-        
+s1 = student("Param")
+print(s1.name) # Param name 
+
+s2 = student("Jenny")
+print(s2.name) # Jenny
