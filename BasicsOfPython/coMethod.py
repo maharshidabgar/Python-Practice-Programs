12e55ef2d89055f9d4c7666639d747fdc0aab55d
maharshidabgar <mahrshidabgar173@gmail.com>
2026-08-13 10:37:33 +0530

    coMethod.py

---
+++ BasicsOfPython/coMethod.py
@@ -0,0 +1,17 @@
+class Stiu:
+
+    college_name = "B.N Patel Science"
+
+    def __init__(self, name, marks):
+
+        self.name = name
+        self.marks = marks
+
+    def welcome():
+
+        print("Jay Mataji Balko !")
+
+s1 = Stiu("Raman", 57)
+
+print(s1.name)
+
