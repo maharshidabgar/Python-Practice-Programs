7bc286c6a9627f7dae25aa232828d8cf777fbef2
maharshidabgar <mahrshidabgar173@gmail.com>
2026-08-13 12:22:42 +0530

    Add pq40.py

--- BasicsOfPython/pq40.py
+++ BasicsOfPython/pq40.py
@@ -1,18 +1,18 @@
 '''Create a Circle class that takes radius. 
 Create a method area() to calculate the area.'''
 
-class Circle:
+class Circle: # Class Declare
 
-    def __init__(self, radius):
+    def __init__(self, radius): # Constructor
 
         self.radius = radius
 
-    def area(self):
+    def area(self): # Method with only Self
 
         area_of_circle = 3.14 * self.radius * self.radius
 
         print("Area of Circle is : ", area_of_circle)
 
-rad = Circle(4)
+rad = Circle(4) # Object Declare
 
-rad.area()
+rad.area() # Main Print object with Method
