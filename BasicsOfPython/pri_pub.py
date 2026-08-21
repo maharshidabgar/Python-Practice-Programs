430409e1b65fa896272cfeffcc0bbd470c8d10c1
maharshidabgar <mahrshidabgar173@gmail.com>
2026-08-21 10:57:27 +0530

    private_var.py

--- BasicsOfPython/pri_pub.py
+++ BasicsOfPython/pri_pub.py
@@ -3,7 +3,7 @@ class Account:
     def __init__(self, acc_no, acc_pass):
 
         self.acc_no = acc_no
-        self.__acc_pass = acc_pass
+        self.__acc_pass = acc_pass # Private Data
 
 
     def reset_pass(self):
---
+++ BasicsOfPython/private_var.py
@@ -0,0 +1,15 @@
+class Student:
+
+    def __init__(self, name, marks):
+        self.name = name
+        self.__marks = marks   # Private data
+
+
+    def reset_var(self):
+
+        print(s1.__marks)
+
+s1 = Student("Maharshi", 85)
+
+print(s1.name)       # Works
+print(s1.reset_var)    # Its Run 
