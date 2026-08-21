d066bb7031473b1dcd6f063c66ef7334370f40f0
maharshidabgar <mahrshidabgar173@gmail.com>
2026-08-21 11:42:10 +0530

    private_var.py

---
+++ BasicsOfPython/inheritence.py
@@ -0,0 +1,26 @@
+class Car:
+
+    color = "red"
+    @staticmethod
+    def start():
+
+        print("Car Started...")
+
+    @staticmethod
+    def stop():
+
+        print("Car Stopped...")
+
+class ToyotaCar(Car): # Inheritence
+
+    def __init__(self, name):
+
+        self.name = name
+
+car1 =  ToyotaCar("Alto")
+
+car2 =  ToyotaCar("Fortuner")
+
+print(car2.color)
+
+print(car2.start())
